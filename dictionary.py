2986c75e732987e577669f31966f90b479638230
Code-Demon07 <24071A05A2@vnrvjiet.in>
2025-02-06 21:38:48 +0530

    Dictionary printing

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+#Create a dictionary, add five key-value pairs, and print them.
+my_dict = {}
+
+my_dict["name"] = "Isagi"
+my_dict["age"] = 18
+my_dict["city"] = "Japan"
+my_dict["profession"] = "Japan U-20 Football team captain"
+my_dict["hobbies"] = ["Football", "Swimming", "Excersicing"]
+
+print("Dictionary:", my_dict)
